ea04d80f0deb38c6f8f06ac0637e4cb97ff335c8
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-05-15 18:15:01 -0400

    commit tree fa998abb3d716df7705d90a38f66d49840f3cab1

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,107 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [myPortfolio, setMyPorfolio] = useState([]); 
+  const [sortList, setSortList] = useState('Alphabetically'); 
+  const [selectedCategory, setSelectedCategory] = useState("Tech");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+    .then((r) => r.json())
+    .then((stocks) => setStocks(stocks));
+  }, []);
+
+
+    // console.log('testing'); // console log's when state is updated
+    
+    // useEffect(() => {
+      //   const sortedTypes = handleFilter()
+      //   setSelectedCategory(sortedTypes);
+      // }, []);
+      
+  //     function handleFilter(stock){
+  //   // let value = e.target.value
+  //     if (selectedCategory === stock.type) {
+  //       return stock.type === 'Tech'
+  //     } else if (selectedCategory === 'Sportswear') {
+  //     return stock.type === 'Sportswear'
+  //     } else if (selectedCategory === 'Finance') {
+  //     return stock.type === 'Finance'
+  //   }
+  //   })
+  // }
+  
+  const sortCategoryList = (e) => {
+    // console.log('sort button clicked');
+    // console.log(e.target.value);
+    setSelectedCategory(e.target.value);
+  }
+  
+  const sortStockList = (e) => {
+    // console.log('sort button clicked');
+    // console.log(e.target.value);
+    setSortList(e.target.value);
+  }
+  
+  const sortByPrice = () => {
+    return [...stocks].sort(function (a, b) {
+      return a.price - b.price;
+    });
+  }
+  
+  const sortAlphabetically = () => {
+    return [...stocks].sort(function(a, b) {
+      const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      
+      // names must be equal
+      return 0;
+    });
+  }
+  
+  let sortedList;
+  if(sortList === 'Alphabetically'){
+    sortedList = sortAlphabetically()
+    // setStocks(sortedList);
+  } else {
+    sortedList = sortByPrice()
+    // setStocks(sortedList);
+  }
+
+  const filterArray = sortedList.filter((stock) => selectedCategory === stock.type)
+
+  const addStock = (stock) => {
+    // console.log('buy')
+    if(!myPortfolio.includes(stock)){
+      const updatedPorfolio = [...myPortfolio, stock]
+      setMyPorfolio(updatedPorfolio)
+    }
+  }
+
+  const sellStock = (stock) => {
+    const updateCart = [...myPortfolio].filter(myPortfolio => myPortfolio.id !== stock.id)
+    setMyPorfolio(updateCart)
+  }
+  
   return (
     <div>
-      <SearchBar />
+      <SearchBar type={sortList} sortStockList={sortStockList} sortList={sortList} sortCategoryList={sortCategoryList} />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={filterArray} handleClick={addStock} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer stocks={myPortfolio} handleClick={sellStock} />
         </div>
       </div>
     </div>
@@ -20,3 +109,12 @@ function MainContainer() {
 }
 
 export default MainContainer;
+
+
+// array1 []
+
+// const a = array1.filter()
+
+// a.filter
+
+// array of obj colors and names, 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar({ type, sortStockList, sortList, sortCategoryList }) {
   return (
     <div>
       <strong>Sort by:</strong>
@@ -9,8 +9,8 @@ function SearchBar() {
           type="radio"
           value="Alphabetically"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={sortList === 'Alphabetically'}
+          onChange={sortStockList}
         />
         Alphabetically
       </label>
@@ -19,15 +19,17 @@ function SearchBar() {
           type="radio"
           value="Price"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={sortList === 'Price'}
+          onChange={sortStockList}
         />
         Price
       </label>
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select 
+        type={type}
+        onChange={sortCategoryList}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
